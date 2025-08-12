909d422b80b8dc09199f181ff3975f2f0c49175f
vg006 <devvg006@gmail.com>
2025-08-12 14:04:02 +0530

    ✨ feat: Initial Commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "FOSS Club CIT Website";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+     	let
+    		pkgs = nixpkgs.legacyPackages.${system};
+     	in {
+        devShells.default = pkgs.mkShell {
+       	  packages = with pkgs; [
+            nodejs_22
+            bun
+         	];
+        };
+      }
+    );
+}
